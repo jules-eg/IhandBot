cc30fb4cb08ea315a81d16f7cb1546c4c7951520
Jules EVENO laptop <Vous@exemple.com>
2025-02-10 03:58:54 +0100

    script

---
+++ script.bash
@@ -0,0 +1,4 @@
+wget https://github.com/mozilla/geckodriver/releases/download/v0.35.0/geckodriver-v0.35.0-linux64.tar.gz
+tar -xvzf geckodriver-v0.35.0-linux64.tar.gz
+chmod +x geckodriver
+pip install -r requirements
