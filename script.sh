5d2b0cbac551d52a3a7b4b4d8fff131d9d650332
Jules EVENO laptop <Vous@exemple.com>
2025-02-10 04:45:43 +0100

    path

--- script.sh
+++ script.sh
@@ -5,5 +5,7 @@ chmod +x geckodriver
 sudo apt-get remove --purge firefox
 sudo apt-get install firefox
 sudo apt install firefox=123.0
-pip install -r requirements.txt
+PATH=$PATH:$(pwd)/geckodriver
+echo $PATH
 ls -al
+pip install -r requirements.txt
