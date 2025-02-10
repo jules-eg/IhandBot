68dbf76b15b8b64cf915a3e0289ecac80e200b30
Jules EVENO laptop <Vous@exemple.com>
2025-02-10 13:27:28 +0100

    passage a chrome

--- script.sh
+++ script.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
-wget https://github.com/mozilla/geckodriver/releases/download/v0.35.0/geckodriver-v0.35.0-linux64.tar.gz
-tar -xvzf geckodriver-v0.35.0-linux64.tar.gz
-chmod +x geckodriver
-apt-get remove --purge firefox -y
-apt install firefox
-PATH=$PATH:$(pwd)
+wget https://chromedriver.storage.googleapis.com/114.0.5735.90/chromedriver_linux64.zip
+unzip chromedriver_linux64.zip
+chmod +x chromedriver
 python3 -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
