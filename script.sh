fc2dfae1f73649240967f8f705cebc56f8526ae4
Jules EVENO laptop <Vous@exemple.com>
2025-02-10 04:23:59 +0100

    apt

--- script.sh
+++ script.sh
@@ -2,4 +2,7 @@
 wget https://github.com/mozilla/geckodriver/releases/download/v0.35.0/geckodriver-v0.35.0-linux64.tar.gz
 tar -xvzf geckodriver-v0.35.0-linux64.tar.gz
 chmod +x geckodriver
+sudo apt-get remove --purge firefox
+sudo apt-get install firefox
+sudo apt install firefox=123.0
 pip install -r requirements.txt
