01d8460c3e5a153c823347e62d3fce3073ea3254
Jules EVENO laptop <Vous@exemple.com>
2025-02-10 13:37:14 +0100

    chromium install

--- script.sh
+++ script.sh
@@ -2,6 +2,7 @@
 wget https://chromedriver.storage.googleapis.com/114.0.5735.90/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip
 chmod +x chromedriver
+apt install chromium
 python3 -m venv .venv
 source .venv/bin/activate
 pip install -r requirements.txt
